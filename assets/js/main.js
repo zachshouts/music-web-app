9c719ecee1b3f3c6d06e955d9a5980d5e11d253f
Greg Trahan <trahan.greg@gmail.com>
2023-01-13 12:37:45 -0600

    format code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,44 @@
-const apikey = '862d8de8876f6a203cf76dea7a7e3cec'
-const button = $('.btn')
+const apikey = "862d8de8876f6a203cf76dea7a7e3cec";
+const button = $(".btn");
 
-let nameArr = localStorage.getItem(JSON.parse('artistHistory'))
-  if (nameArr === null) {
-    nameArr = []
-  }
-  
-
-button.on('click', function (event) {
-  searchArtist()
-  event.preventDefault()
-})
-
-function searchArtist() {
-  let artist = ""
-  const searchBox = $('.form-control')
-  artist = searchBox.val()
-  let artistData = findArtist(artist)
+let nameArr = JSON.parse(localStorage.getItem("artistHistory"));
+if (nameArr === null) {
+  nameArr = [];
 }
 
+function loadHistory() {}
+
+button.on("click", function (event) {
+  searchArtist();
+  event.preventDefault();
+});
+
+function searchArtist() {
+  let artist = "";
+  const searchBox = $(".form-control");
+  artist = searchBox.val();
+  let artistData = findArtist(artist);
+}
 
 function findArtist(artist) {
-  const endpoint = `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=${apikey}&format=json&limit=1`
+  const endpoint = `http://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums&artist=${artist}&api_key=${apikey}&format=json&limit=5`;
 
   fetch(endpoint)
     .then(function (response) {
-      return response.json()
+      return response.json();
     })
 
     //Retrieve artist data
     .then(function (data) {
+      console.log(data);
 
-    storeNames()
+      storeNames();
 
-      function storeNames(){
-        let artistName = (data.results.artistmatches.artist[0].name)
-        nameArr.push(artistName)
-        console.log(nameArr)
-        localStorage.setItem('artistHistory', JSON.stringify(nameArr))
+      function storeNames() {
+        let artistName = data.topalbums.attr.artist;
+        nameArr.push(artistName);
+        console.log(nameArr);
+        localStorage.setItem("artistHistory", JSON.stringify(nameArr));
       }
-
-    })
+    });
 }
-
-
-
-
-
