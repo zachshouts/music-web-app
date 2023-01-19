19fbb91fb303c396792a148aaa81b7b6ca90beaf
Cong Thanh Pham <chrispham211@gmail.com>
2023-01-19 13:54:51 -0600

    Added fading effect to text, UI responsive for mobile

--- assets/js/top-content.js
+++ assets/js/top-content.js
@@ -1,16 +1,16 @@
 const apikey = "862d8de8876f6a203cf76dea7a7e3cec";
 
 const imgArtistArr = [
-  "../assets/img/artists/weeknd.png",
   "../assets/img/artists/taylorswift.png",
-  "../assets/img/artists/SZA.png",
-  "../assets/img/artists/kendricklamar.png",
-  "../assets/img/artists/Drake.png",
   "../assets/img/artists/kanye.png",
-  "../assets/img/artists/arcticmonkeys.png",
-  "../assets/img/artists/ariana.png",
   "../assets/img/artists/lanadelray.png",
+  "../assets/img/artists/arcticmonkeys.png",
+  "../assets/img/artists/Drake.png",
+  "../assets/img/artists/weeknd.png",
+  "../assets/img/artists/ariana.png",
+  "../assets/img/artists/kendricklamar.png",
   "../assets/img/artists/tyler.png",
+  "../assets/img/artists/SZA.png",
 ];
 
 const topArticlesElement = $("#top-article-list");
@@ -28,6 +28,8 @@ const imgTrackArr = [
   "../assets/img/tracks/nobodygetsme.png",
 ];
 
+const w = window.innerWidth;
+
 function getTopArtists() {
   const endpoint = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apikey}&format=json&limit=10`;
 
@@ -38,10 +40,16 @@ function getTopArtists() {
     //Retrieve top artists data
     .then(function (data) {
       const artistList = data.artists.artist;
+
+      artistList.sort((prev, next) => next.playcount - prev.playcount);
+
+      console.log("after sort", artistList);
+
       artistList.forEach((a, index) => {
         topArticlesElement.append(
           $("<div/>", {
-            class: "col-1 d-flex align-items-center",
+            class:
+              "col-12 col-sm-1 d-flex justify-content-center justify-content-sm-start align-items-center fs-1",
             text: `${index + 1}`,
           })
         );
@@ -50,7 +58,8 @@ function getTopArtists() {
         topArticlesElement.append(
           $("<div/>", {
             id: `artist-img-${index}`,
-            class: "col-2 d-flex align-items-center",
+            class:
+              "col-12 col-sm-2 d-flex justify-content-center justify-content-sm-start align-items-center",
           }).append(
             $("<img/>", { class: "artist-img" }).attr(
               "src",
@@ -62,22 +71,26 @@ function getTopArtists() {
         topArticlesElement.append(
           $("<div/>", {
             id: `artist-name-${index}`,
-            class: "col-3 d-flex align-items-center",
+            class:
+              "col-12 col-sm-3 d-flex justify-content-center justify-content-sm-start align-items-center text-danger",
             text: `${a.name}`,
           })
         );
 
         topArticlesElement.append(
           $("<div/>", {
-            class: "col-3 d-flex align-items-center",
-            text: `${a.listeners}`,
+            class:
+              "col-12 col-sm-3 d-flex justify-content-center justify-content-sm-start align-items-center text-warning",
+            text: (w > 576 ? "" : "Listeners:") + ` ${a.listeners}`,
           })
         );
 
         topArticlesElement.append(
           $("<div/>", {
-            class: "col-3 d-flex align-items-center",
-            text: `${a.playcount}`,
+            class: `col-12 col-sm-3 d-flex justify-content-center justify-content-sm-start align-items-center text-playcount-${
+              artistList.length - index
+            }`,
+            text: (w > 576 ? "" : "Playcount:") + ` ${a.playcount}`,
           })
         );
 
